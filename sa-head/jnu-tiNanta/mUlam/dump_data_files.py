3b2080a3f3f6fe1081e691230b72d0d0b20a3ccf
vvasuki <vvasuki@localhost>
2015-02-22 11:16:13 -0800

    asdf

--- sa-head/jnu-tiNanta/mUlam/dump_data_files.py
+++ sa-head/jnu-tiNanta/mUlam/dump_data_files.py
@@ -1,6 +1,6 @@
 import subprocess
 
-for i in range(1,439):
+for i in range(1,451):
   url = "http://sanskrit.jnu.ac.in/tinanta/tinanta.jsp?t=%d" % i
   cmd = "lynx -dump " + url + "> ~/tinanta/%d" % i
   print cmd
