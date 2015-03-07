f2083c588ad9bbc6e214b49e02348e04b3327aa0
vvasuki <vvasuki@localhost>
2015-03-07 10:14:29 -0800

    asdf

--- sa-head/kalpadruma-sa/mUlam/make_db.py
+++ sa-head/kalpadruma-sa/mUlam/make_db.py
@@ -2,13 +2,9 @@
 # Prerequisite: sudo easy_install regex
 import regex
 import sys
+import collections
 
-for line in sys.stdin:
-  head_v1, value = line.split("\t")
-  headwords = regex.sub(" +", " ", head_v1).strip().split(" ")
-
-  headwords = map(lambda headword : regex.sub(r'\p{P}+', "", headword).strip(), headwords)
-  headwords = filter(lambda headword : headword != "", headwords)
+def print_entry(headwords, value):
   printed_words = set()
   for headword in headwords:
     # print headword
@@ -17,4 +13,24 @@ for line in sys.stdin:
       printed_words.add(headword)
       # just print the first headword
       break
+  
+
+word_count = collections.Counter()
+test_lines = ["अघ अघ त् क तत्कृतौ . इति कविकल्पद्रुमः .. तत्-कृतिः पापकृर्तिः . अघयति व्याधः . कर्म्मणो-ऽर्थमध्यपाठादकर्म्मकोऽयं . तथा च	अघ अघ त् क तत्कृतौ . इति कविकल्पद्रुमः .. तत्-कृतिः पापकृर्तिः . अघयति व्याधः . कर्म्मणो-ऽर्थमध्यपाठादकर्म्मकोऽयं . तथा च, --“ धातोरर्थान्तरे वृत्ते धात्वर्थेनोपसङ्ग्रहात् .प्रसिद्धेरविवक्षातः कर्म्मणोऽकर्म्मिका क्रिया” ..इति गोयीचन्द्रः .. धात्वर्थेन सह कर्म्मण उप-सङ्ग्रहादित्यर्थः . क्रमेणोदाहरणानि . नदीवहति क्षरतीत्यर्थः . अघयति व्याधः . भवतिघटः . आहते जनः . इति दुर्गादासः .."]
+for line in sys.stdin:
+# for line in test_lines:
+  head_v1, value = line.split("\t")
+  headwords = regex.sub(" +", " ", head_v1).strip().split(" ")
+
+  headwords = map(lambda headword : regex.sub(r'\p{P}+', "", headword).strip(), headwords)
+  headwords = filter(lambda headword : headword != "", headwords)
+  word_count[len(set(headwords))] += 1
+  if (len(set(headwords)) > 4):
+       #print line.strip()
+       #print headwords
+       pass
+  print_entry(headwords, value)
+  
+
+# print word_count
 
