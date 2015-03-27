6a7c5fc8fc66030f22e099d33e25ef9bf77b6732
vvasuki <vishvas.vasuki@gmail.com>
2015-03-26 20:06:45 -0700

    asdf

--- en-head/shabda-sAgara/mUlam/make_tsv.scala
+++
@@ -1,7 +0,0 @@
-/**
- * Created by vvasuki on 3/14/15.
- */
-
-import sanskritnlp.transliteration._
-println("hello");
-
--- en-head/shabda-sAgara/mUlam/to_devanAgarI.scala
+++ en-head/shabda-sAgara/mUlam/to_devanAgarI.scala
@@ -1,4 +1,16 @@
-/**
- * Created by vvasuki on 3/26/15.
- */
+import scala.io.Source
+import java.io._
+import sanskritnlp.transliteration._
 
+val infile = "/home/vvasuki/stardict-sanskrit/en-head/shabda-sAgara/mUlam/shs_orig_utf8_v1.tsv"
+val outfile = "/home/vvasuki/stardict-sanskrit/sa-head/shabda-sAgara/shabda-sAgara.tsv"
+val src = Source.fromFile(infile, "utf8")
+val destination = new PrintWriter(new File(outfile))
+
+val pattern = """\{#(.+?)#\}""".r
+src.getLines.foreach(line => {
+  val newLine = pattern.replaceAllIn(line, _ match {
+    case pattern(hk) => harvardKyoto.toDevanagari(hk)})
+  // println(line)
+  destination.println(newLine)
+})
