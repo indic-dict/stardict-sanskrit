57d24d94a01b118315d3f79118f40b4102edfc09
Vishvas Vasuki <vvasuki@google.com>
2015-07-10 16:41:27 -0700

    asdf

--- sync_all_repos.sh
+++ sync_all_repos.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-GIT_MODE=$1
+GIT_MODE=$1 # gp or git pull
 set -o verbose
 CUR_DIR=$(pwd)
 REPOS=(~/stardict-sanskrit/ ~/sanskritnlpjava ~/stardict-sanskrit)
@@ -7,6 +7,6 @@ INFREQUENT_REPOS=(~/stardict-pali/ ~/stardict-hindi/ ~/stardict-kannada/)
 for repo in "${REPOS[@]}"; 
 do echo processing: $repo;
 cd $repo
-git $GIT_MODE
+$GIT_MODE
 done;
 cd $CUR_DIR
