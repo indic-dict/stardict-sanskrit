2d9232349f7389904a3e106c5b5972e199167a85
vvasuki <vishvas.vasuki@gmail.com>
2015-08-13 20:17:36 -0700

    asdf

--- sync_all_repos.sh
+++ sync_all_repos.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 set -o verbose
+shopt -s expand_aliases
+source ~/.bash_aliases
 CUR_DIR=$(pwd)
 REPOS=(~/stardict-sanskrit/ ~/sanskritnlpjava ~/m17n-db)
 INFREQUENT_REPOS=(~/stardict-pali/ ~/stardict-hindi/ ~/stardict-kannada/)
 for repo in "${REPOS[@]}"; 
 do echo processing: $repo;
 cd $repo
-git pull
+$1
 done;
 cd $CUR_DIR
