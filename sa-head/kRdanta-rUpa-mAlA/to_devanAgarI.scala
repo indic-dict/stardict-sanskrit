a7390aa7ae05d31611c17687b967038635363122
vvasuki <vishvas.vasuki@gmail.com>
2016-02-16 14:06:21 -0800

    asdf

--- sa-head/kRdanta-rUpa-mAlA/to_devanAgarI.scala
+++ sa-head/kRdanta-rUpa-mAlA/to_devanAgarI.scala
@@ -2,7 +2,7 @@ import scala.io.Source
 import java.io._
 import sanskritnlp.transliteration._
 val infileStr = "/home/vvasuki/stardict-sanskrit/sa-head/kRdanta-rUpa-mAlA/mUlam/kRdanta-rUpa-mAlA.tsv"
-val outfileStr = "/home/vvasuki/stardict-sanskrit/sa-head/kRdanta-rUpa-mAlA/kRdanta-rUpa-mAlA.tsv"
+val outfileStr = "/home/vvasuki/stardict-sanskrit/sa-head/kRdanta-rUpa-mAlA/kRdanta-rUpa-mAlA-test.tsv"
 val src = Source.fromFile(infileStr, "utf8")
 val outFileObj = new File(outfileStr)
 new File(outFileObj.getParent).mkdirs
@@ -13,7 +13,7 @@ src.getLines.foreach(line => {
   newLine = harvardKyoto.restoreEscapeSequences(newLine)
   destination.println(newLine)
   // println(line)
-  println(newLine)
+  // println(newLine)
 })
 destination.close()
 println("")
