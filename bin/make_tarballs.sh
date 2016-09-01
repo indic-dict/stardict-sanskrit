3f44b5eddfa1276736a4191f317dee5d5d1a327d
Vishvas Vasuki <vvasuki@google.com>
2016-09-01 16:35:08 -0700

    asdf

--- bin/make_tarballs.sh
+++ bin/make_tarballs.sh
@@ -8,10 +8,14 @@ for dir in */
 do
 	base=$(basename "$dir")
 	cd $dir
-	timestamp=$(stat -c %y *.babylon| tr " " "_"|tr ":" "-"|cut -d'.' -f 1)
+	timestamp=$(stat -c %y *.babylon_final| tr " " "_"|tr ":" "-"|cut -d'.' -f 1)
+	if [[ -z $timestamp ]]; then
+	  timestamp=$(stat -c %y *.babylon| tr " " "_"|tr ":" "-"|cut -d'.' -f 1)
+	fi
 	if [[ -z $timestamp ]]; then
 	  timestamp=$(stat -c %y *.dict| tr " " "_"|tr ":" "-"|cut -d'.' -f 1)
 	fi
+	
 	tarfile="${base}__${timestamp}.tar.gz"
 	if [ -f "${base}.dict.dz" ]; then
 	  tar -czf "${tarfile}" `ls *.idx *.dz *.ifo *.syn`
