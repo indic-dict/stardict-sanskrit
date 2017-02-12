dcda290680d7748e2dec5589eaa7c2ad1af9d963
vvasuki <vishvas.vasuki@gmail.com>
2017-02-11 21:23:58 -0800

    asdf

--- sa-vyAkaraNa/abhyankara-grammar/scripts/make_babylon.scala
+++ sa-vyAkaraNa/abhyankara-grammar/scripts/make_babylon.scala
@@ -3,6 +3,8 @@
 
 import java.io._
 
+import org.slf4j.LoggerFactory
+
 import scala.io.Source
 val infileStr = "/home/vvasuki/stardict-sanskrit/sa-vyAkaraNa/abhyankara-grammar/abhyankara-grammar.txt"
 val outfileStr = "/home/vvasuki/stardict-sanskrit/sa-vyAkaraNa/abhyankara-grammar/abhyankara-grammar.babylon"
@@ -11,21 +13,37 @@ val outFileObj = new File(outfileStr)
 new File(outFileObj.getParent).mkdirs
 val destination = new PrintWriter(outFileObj)
 
-var outEntries = src.getLines.map{ line => {
-  // println(line)
-  var output = line
-  var pattern = "^\\{\\{.+\\}\\}".r
-  output = output.replaceAll(pattern.toString(), "")
-//  output = pattern.replaceAllIn(output, _ match { case pattern(key, c2, c3, meaning) => {
-//    f"######$key\n$key $c2$c3<br>$meaning"
-//  }})
-  // println(output)
-  output
-}}.mkString("\n").split("######")
+val log = LoggerFactory.getLogger("abhyankarabot")
 
-outEntries.foreach(entry => {
-  destination.println(entry)
-  destination.println("")
+var outText = src.getLines.mkString("\n")
+outText = outText.replaceAll("<noinclude>.+</noinclude>", "")
+outText = outText.replaceAll("\\{\\{outdent\\|", "")
+outText = outText.replaceAll("(left|right|center)[ =]+[']+.+?[']+", "")
+Range(1,10).foreach(_ => {
+  outText = outText.replaceAll("(?s)\\{\\{[^}]+?\\}\\}", "")
+})
+outText = outText.trim
+outText = outText.replaceAll(" ", " ")
+outText = outText.replaceAll("\n[^'\n]", " ")
+//outText = outText.trim
+
+val entryLines = outText.split("\n")
+entryLines.foreach(destination.println)
+destination.close()
+exit
+
+var failedLineCount = 0
+entryLines.foreach(entryLine => {
+  if (entryLine.split("'''").length != 3) {
+    log warn "Could not parse: " + entryLine
+    failedLineCount += 1
+  } else {
+    val entries = entryLine.split("'''")
+    destination.println(entries(1).trim)
+    destination.println(entries(2).trim)
+    destination.println("")
+  }
 })
 destination.close()
-println("Done!")
+log warn "Failed on " + failedLineCount + " lines"
+log info "Done!"
