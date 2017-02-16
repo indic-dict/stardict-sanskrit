70cf20ae93c6e735013822c0b3f6fb7a6f515d02
vvasuki <vishvas.vasuki@gmail.com>
2017-02-15 19:13:25 -0800

    asdf

--- sa-kAvya/dcs-frequency/scripts/mkBabylon.scala
+++ sa-kAvya/dcs-frequency/scripts/mkBabylon.scala
@@ -1,11 +1,49 @@
-/**
-  */
+// To run:
+// PATH_TO_SANSKRITNLPJAVA=/home/vvasuki/sanskritnlpjava/target
+// scala -classpath "$PATH_TO_SANSKRITNLPJAVA/sanskritnlp-1.0-SNAPSHOT/WEB-INF/lib/*:$PATH_TO_SANSKRITNLPJAVA/sanskritnlp-1.0-SNAPSHOT/WEB-INF/classes" stardict-sanskrit/sa-kAvya/dcs-frequency/scripts/mkBabylon.scala
+
+
 
 import java.io._
+import net.liftweb.json.DefaultFormats
+import net.liftweb.json._
 
 import org.slf4j.LoggerFactory
 
 import scala.io.Source
+val textFileStr = "/home/vvasuki/stardict-sanskrit/sa-kAvya/dcs-frequency/dcs-frequency.csv"
+
+case class Text (
+  Number:Int,
+  TextID:Int,
+  TextName: String,
+  Authors: String,
+  Era: String,
+  Subject: String,
+  DatingStart:Int,
+  DatingEnd:Int,
+  EstDateLesser:Int,
+  EstDateGreater:Int,
+  FIELD11: String,
+  Completed: String,
+  Source: String,
+  TextorCommentary: String,
+  SourceURL: String,
+  TextDataURL: String,
+  WikiLink: String,
+  DateCommonlyAccepted: String,
+  MeaningOfName: String,
+  WikiSynopsis: String
+)
+
+def GetTexts = {
+  implicit val formats = DefaultFormats
+  val src = Source.fromFile(textFileStr, "utf8")
+  val jsonStr = src.getLines().mkString("\n")
+  val textListJson = parse(jsonStr)
+
+}
+
 val infileStr = "/home/vvasuki/stardict-sanskrit/sa-kAvya/dcs-frequency/dcs-frequency.csv"
 val outfileStr = "/home/vvasuki/stardict-sanskrit/sa-kAvya/dcs-frequency/dcs-frequency.babylon"
 val src = Source.fromFile(infileStr, "utf8")
@@ -22,9 +60,10 @@ src.getLines.foreach(inLine => {
     log warn "Could not parse: " + entry.mkString(",")
     failedLineCount += 1
   } else {
-    
+
   }
 })
+
 //outText = outText.replaceAll("\\{\\{outdent\\|", "")
 //outText = outText.replaceAll("(left|right|center)[ =]+[']+.+?[']+", "")
 //Range(1,10).foreach(_ => {
