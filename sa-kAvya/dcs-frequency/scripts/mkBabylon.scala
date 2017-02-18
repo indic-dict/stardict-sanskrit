f3284240aa6c1eb814de60ca2b879cdd842eb040
vvasuki <vishvas.vasuki@gmail.com>
2017-02-17 22:24:15 -0800

    asdf

--- sa-kAvya/dcs-frequency/scripts/mkBabylon.scala
+++ sa-kAvya/dcs-frequency/scripts/mkBabylon.scala
@@ -6,14 +6,40 @@ import java.io._
 
 import org.slf4j.LoggerFactory
 
+import sanskritnlp.transliteration._
+import scala.collection.mutable
 import scala.io.Source
 val log = LoggerFactory.getLogger("dcsfr")
 
 val textFileStr = "/home/vvasuki/stardict-sanskrit/sa-kAvya/dcs-frequency/text2.json"
 
+// Example data:
+//{
+//  "Number":1,
+//  "TextID (DCS)":15,
+//  "Text Name":"Abhidharmakośa",
+//  "Author(s)":"Vasubandhu",
+//  "Era":"classical",
+//  "Subject":"Buddhist",
+//  "Dating Start":400,
+//  "Dating End":500,
+//  "Est Date Lesser":400,
+//  "Est Date Greater":500,
+//  "FIELD11":"OK",
+//  "Completed":"",
+//  "Source":"DCS",
+//  "Text or Commentary":"Text",
+//  "Source URL":"http://kjc-fs-cluster.kjc.uni-heidelberg.de/dcs/index.php?contents=texte&IDTextDisplay=15",
+//  "Text Data URL":"https://goo.gl/kv94Ki",
+//  "Wiki Link":"https://en.wikipedia.org/wiki/Abhidharma-kosa",
+//  "Date Commonly Accepted":"",
+//  "Meaning of Name":"Treasury of Supreme Truth",
+//  "Wiki Synopsis":"Abhidharma-kośa (Sanskrit; Tibetan: chos mngon pa'i mdzod; English: Treasury of Abhidharma) is a key text on the abhidharma written in Sanskrit verse by Vasubandhu, in the 4th or 5th century. It summarizes the Sarvāstivādin tenets in eight chapters with a total of around 600 verses. The text was widely respected, and used by schools of Mahayana Buddhism in India, Tibet, and the Far East."
+//},
+
 case class Text (
   Number:Int,
-  TextID:Int,
+  TextID:String,
   TextName: String,
   Authors: String,
   Era: String,
@@ -34,17 +60,44 @@ case class Text (
   WikiSynopsis: String
 )
 
+val texts = mutable.HashMap[String, Text]()
+
 def GetTexts = {
-  import net.liftweb.json.DefaultFormats
-  import net.liftweb.json._
+  import net.liftweb.json.{DefaultFormats, _}
   implicit val formats = DefaultFormats
   val src = Source.fromFile(textFileStr, "utf8")
   val jsonStr = src.getLines().mkString("\n")
   val textListJson = parse(jsonStr)
-  for (textJason <- (textListJson \\ "Texts").children) {
-    val text = textJason.extract[Text]
-    log info text.toString
+  // Fails: val texts = textListJson.extract[List[Text]]
+  for (textJason <- (textListJson \ "Texts").children) {
+    log info textJason.toString
+    val tname = textJason \ "Text Name"
+//    log info tname.toString
+    val text = Text(
+      (textJason \ "Number").extract[Int],
+      (textJason \ "TextID (DCS)").extract[Int].toString,
+      (textJason \ "Text Name").extract[String],
+      (textJason \ "Author(s)").extract[String],
+      (textJason \ "Era").extract[String],
+      (textJason \ "Subject").extract[String],
+      (textJason \ "Dating Start").extract[Int],
+      (textJason \ "Dating End").extract[Int],
+      (textJason \ "Est Date Lesser").extract[Int],
+      (textJason \ "Est Date Greater").extract[Int],
+      (textJason \ "FIELD11").extract[String],
+      (textJason \ "Completed").extract[String],
+      (textJason \ "Source").extract[String],
+      (textJason \ "Text or Commentary").extract[String],
+      (textJason \ "Source URL").extract[String],
+      (textJason \ "Text Data URL").extract[String],
+      (textJason \ "Wiki Link").extract[String],
+      (textJason \ "Date Commonly Accepted").extract[String],
+      (textJason \ "Meaning of Name").extract[String],
+      (textJason \ "Wiki Synopsis").extract[String]
+    )
+    texts(text.TextID) = text
   }
+  log info f"Gathered ${texts.size}%d texts"
 }
 
 GetTexts
@@ -57,13 +110,29 @@ new File(outFileObj.getParent).mkdirs
 val destination = new PrintWriter(outFileObj)
 
 var failedLineCount = 0
+var ignoredLineCount = 0
 src.getLines.foreach(inLine => {
   val entry = inLine.split(",")
   if(entry.length != 5) {
     log warn "Could not parse: " + entry.mkString(",")
     failedLineCount += 1
+  } else if(entry(4) == "0") {
+    log warn "Ignoring: " + entry.mkString(",")
+    ignoredLineCount += 1
   } else {
+    val word = entry(0)
+    val grammar_hint = entry(1) + " " + entry(2)
+    //    log debug (word)
+    //    log debug entry(3).split(";").mkString(" ")
+    val textOccurances = entry(3).split(";").map(_.split(":"))
+    val totalFrequency = entry(4)
+    var occuranceString = f"Occurances:<BR>"
+    occuranceString += textOccurances.sortBy(_(1).toInt).map(x => texts(x(0)).TextName + ": " + x(1)).mkString("<BR>")
+    val dict_entry = f"$word%s<BR>$grammar_hint%s<BR>$occuranceString%s<BR>Total: $totalFrequency%s<BR>"
 
+    destination.println(iast.toDevanagari(word).get)
+    destination.println(dict_entry)
+    destination.println()
   }
 })
 
@@ -82,5 +151,6 @@ src.getLines.foreach(inLine => {
 //entryLines.foreach(destination.println)
 destination.close()
 log warn "Failed on " + failedLineCount + " lines"
+log warn "Ignored " + ignoredLineCount + " lines"
 log info "Done!"
 
