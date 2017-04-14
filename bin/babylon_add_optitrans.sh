baf4202f6404a9fd1f20559d20cf48db56dddebb
vishvAsaH <vishvas.vasuki@gmail.com>
2017-04-14 11:00:53 -0700

    * Fix classpath. * Delete outdated intellij module files.

--- bin/babylon_add_optitrans.sh
+++ bin/babylon_add_optitrans.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 # STARDICT_SANSKRIT_SCALA=`dirname $0`
 PATH_TO_SANSKRITNLPJAVA=~/sanskritnlpjava
-STARDICT_SANSKRIT_SCALA=~/sanskritnlpjava/out/production/stardict_sanskrit_bin
-scala -classpath "$STARDICT_SANSKRIT_SCALA:$PATH_TO_SANSKRITNLPJAVA/out/*" -e "stardict_sanskrit.batchProcessor.addOptitrans(\"$1\".replace(\"DICTS=\", \"\"))"
+scala -classpath "$PATH_TO_SANSKRITNLPJAVA/out/*" -e "stardict_sanskrit.batchProcessor.addOptitrans(\"$1\".replace(\"DICTS=\", \"\"))"
--- bin/tsv_to_stardict.sh
+++ bin/tsv_to_stardict.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 PATH_TO_SANSKRITNLPJAVA=~/sanskritnlpjava
-STARDICT_SANSKRIT_SCALA=~/sanskritnlpjava/out/production/stardict_sanskrit_bin
 BABYLON_BINARY=`echo ~/stardict/tools/src/babylon`
-scala -classpath "$STARDICT_SANSKRIT_SCALA:$PATH_TO_SANSKRITNLPJAVA/out/*" -e "stardict_sanskrit.batchProcessor.makeStardict(\"$1\".replace(\"DICTS=\", \"\"), \"$BABYLON_BINARY\")"
+scala -classpath "$PATH_TO_SANSKRITNLPJAVA/out/*" -e "stardict_sanskrit.batchProcessor.makeStardict(\"$1\".replace(\"DICTS=\", \"\"), \"$BABYLON_BINARY\")"
