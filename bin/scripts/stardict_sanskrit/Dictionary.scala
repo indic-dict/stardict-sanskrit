0286b80620f7d0369628607a12bdc21b7331d9de
vishvAsaH <vishvas.vasuki@gmail.com>
2017-04-25 14:00:36 -0700

    Generate tars, update script to generate tars to remove outdated tar files and handle cases where there is no babylon file.

--- bin/scripts/stardict_sanskrit/Dictionary.scala
+++ bin/scripts/stardict_sanskrit/Dictionary.scala
@@ -45,27 +45,33 @@ class Dictionary(val name: String) {
   }
 
 
-  def getBabylonFile(): File = {
+  def getFinalBabylonFile(): File = {
     if (babylonFinalFile.isDefined) {
       babylonFinalFile.get
-    } else {
+    } else if (babylonFile.isDefined) {
       babylonFile.get
+    } else {
+      null
     }
   }
 
   def ifoFileNewerThanBabylon(): Boolean = {
-    val babFile = getBabylonFile
+    val babFile = getFinalBabylonFile
     ifoFile.isDefined && (ifoFile.get.lastModified > babFile.lastModified)
   }
 
-  def getBabylonTimestampString(): String = {
+  def getBabylonOrIfoTimestampString(): String = {
     // Format: dhAtupATha-sa__2016-02-20_16-15-35.tar.gz
     val format = new java.text.SimpleDateFormat("yyyy-MM-dd_HH-mm-ss")
-    format.format(getBabylonFile.lastModified)
+    if (getFinalBabylonFile != null) {
+      format.format(getFinalBabylonFile.lastModified)
+    } else {
+      format.format(ifoFile.get.lastModified)
+    }
   }
 
   def makeStardictFromBabylonFile(babylon_binary: String) = {
-    val babFile = getBabylonFile
+    val babFile = getFinalBabylonFile
     log info (f"Making stardict from: ${babFile.getCanonicalPath}")
     s"$babylon_binary ${babFile.getCanonicalPath}".!
     dictFile = dirFile.listFiles.map(_.getCanonicalFile).filter(_.getName.matches(s".*/?${dirName}.dict")).headOption
@@ -74,7 +80,7 @@ class Dictionary(val name: String) {
     }
   }
 
-  def getExpectedTarFileName(sizeMbString: String = "unk"): String = s"${dirName}__${getBabylonTimestampString}__${sizeMbString}MB.tar.gz"
+  def getExpectedTarFileName(sizeMbString: String = "unk"): String = s"${dirName}__${getBabylonOrIfoTimestampString}__${sizeMbString}MB.tar.gz"
   def getTarDirFile = new File(dirFile.getParentFile.getCanonicalPath, "/tars")
 
   def tarFileMatchesBabylon(): Boolean = {
@@ -87,6 +93,7 @@ class Dictionary(val name: String) {
       tarFile.get.delete()
     }
     val targetTarFile = new File(getTarDirFile.getCanonicalPath, getExpectedTarFileName())
+    targetTarFile.getParentFile.mkdirs
     val filesToCompress = dirFile.listFiles.map(_.getCanonicalPath).filter(x => x.matches(".*\\.ifo|.*\\.idx|.*\\.dz|.*\\.ifo|.*\\.syn"))
     val command = s"tar --transform s/.*\\///g -czf ${targetTarFile.getCanonicalPath} ${filesToCompress.mkString(" ")}"
     log info command
@@ -118,12 +125,12 @@ object batchProcessor {
     return directories.toList
   }
 
-  def getMatchingDictionaries(file_pattern: String = ".*") = getMatchingDirectories(file_pattern).map(new Dictionary(_)).filter(_.babylonFile.isDefined)
+  def getMatchingDictionaries(file_pattern: String = ".*") = getMatchingDirectories(file_pattern).map(new Dictionary(_))
 
   def addOptitrans(file_pattern: String = ".*") = {
     log info "=======================Adding optitrans headwords, making final babylon file."
     val files_to_ignore = Set("spokensanskrit.babylon")
-    var dictionaries = getMatchingDictionaries(file_pattern)
+    var dictionaries = getMatchingDictionaries(file_pattern).filter(_.babylonFile.isDefined)
     log info (s"Got ${dictionaries.length} babylon files")
     var dictsToIgnore = dictionaries.filter(_.babylonFinalFileNewerThanBabylon())
     if (dictsToIgnore.nonEmpty) {
@@ -147,7 +154,7 @@ object batchProcessor {
 
   def makeStardict(file_pattern: String = ".*", babylon_binary: String) = {
     log info "=======================makeStardict"
-    var dictionaries = getMatchingDictionaries(file_pattern)
+    var dictionaries = getMatchingDictionaries(file_pattern).filter(_.babylonFile.isDefined)
 
     log info (s"Got ${dictionaries.filter(_.babylonFinalFile.isDefined).length} babylon_final files")
     log info (s"Got ${dictionaries.filter(x => x.babylonFile.isDefined && !x.babylonFinalFile.isDefined).length}  dicts without babylon_final files but with babylon file.")
@@ -161,6 +168,7 @@ object batchProcessor {
 
   def writeTarsList(tarDestination: String, urlBase: String) = {
     val outFileObj = new File(tarDestination + "/tars.MD")
+    outFileObj.getParentFile.mkdirs
     val destination = new PrintWriter(outFileObj)
     val urlBaseFinal = urlBase.replaceAll("/$", "")
     outFileObj.getParentFile.listFiles().map(_.getCanonicalFile).filter(_.getName.endsWith("tar.gz")).toList.sorted.foreach(x => {
@@ -172,9 +180,19 @@ object batchProcessor {
   def makeTars(urlBase: String, file_pattern: String = ".*") = {
     log info "=======================makeTars"
     // Get timestamp.
-    var dictionaries = getMatchingDictionaries(file_pattern)
+    var dictionaries = getMatchingDictionaries(file_pattern).filter(_.ifoFile.isDefined)
     log info (s"Got ${dictionaries.filter(_.tarFile.isDefined).length} tar files")
     log info (s"Got ${dictionaries.filter(x => x.ifoFile.isDefined && !x.tarFile.isDefined).length}  dicts without tarFile files but with ifo file.")
+    if (file_pattern == ".*" && dictionaries.nonEmpty) {
+      val tarDirFile = dictionaries.head.getTarDirFile
+      val excessTarFiles = tarDirFile.listFiles.filterNot(x => {
+        val name = x.getName
+        name != "tars.MD" && dictionaries.filter(_.tarFile.isDefined).map(_.tarFile.get.getCanonicalPath).contains(x.getCanonicalPath)
+      })
+      log warn s"Removing ${excessTarFiles.length} excessTarFiles"
+      excessTarFiles.foreach(_.delete())
+      writeTarsList(dictionaries.head.getTarDirFile.getCanonicalPath, urlBase)
+    }
     var dictsToIgnore = dictionaries.filter(_.tarFileNewerThanIfo())
     if (dictsToIgnore.nonEmpty) {
       log warn s"Ignoring these files, whose dict files seem updated: " + dictsToIgnore.mkString("\n")
@@ -185,7 +203,7 @@ object batchProcessor {
     log info(s"got ${dictionaries.length} dictionaries which need to be updated.")
     dictionaries.foreach(_.makeTar)
 
-    if (dictionaries.size > 0) {
+    if (dictionaries.nonEmpty) {
       writeTarsList(dictionaries.head.getTarDirFile.getCanonicalPath, urlBase)
     }
   }
