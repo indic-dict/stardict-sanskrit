3089d47693508d16ab81b62f98f2833757e77334
vishvAsaH <vishvas.vasuki@gmail.com>
2017-05-15 15:42:31 -0700

    Add jar artifact.

--- bin/update_tars_md.sh
+++ bin/update_tars_md.sh
@@ -2,6 +2,5 @@
 PATH_TO_SANSKRITNLPJAVA=~/sanskritnlpjava
 BABYLON_BINARY=`echo ~/stardict/tools/src/babylon`
 PATH_TO_JARS=~/stardict-sanskrit
-echo scala -classpath "$PATH_TO_JARS/bin/artifacts/*" -e "stardict_sanskrit.batchProcessor.writeTarsList(\"$1\", \"$2\")"
-scala -classpath "$PATH_TO_JARS/bin/artifacts/stardict-sanskrit.jar" -e "stardict_sanskrit.tarProcessor.writeTarsList(\"$1\", \"$2\")"
+scala -classpath "$PATH_TO_JARS/bin/artifacts/*" -e "stardict_sanskrit.tarProcessor.writeTarsList(\"$1\", \"$2\")"
 
