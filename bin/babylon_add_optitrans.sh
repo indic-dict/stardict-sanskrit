65914d40d8bb959e7d8adfb9a2c1cbbd7df4cc9a
vvasuki <vishvas.vasuki@gmail.com>
2019-05-14 15:17:54 -0700

    Auto build

--- bin/babylon_add_optitrans.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# STARDICT_SANSKRIT_SCALA=`dirname $0`
-PATH_TO_JARS=~/dict-tools
-java -cp "$PATH_TO_JARS/bin/artifacts/dict-tools.jar" stardict_sanskrit.commandInterface addOptitrans $1
