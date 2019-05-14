147b907d9e91fb9bb83715697593089836a2dfc8
vvasuki <vishvas.vasuki@gmail.com>
2019-05-13 23:11:46 -0700

    Auto build

--- bin/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-## Build the dictionary archives.
-sudo wget https://github.com/sanskrit-coders/dict-tools/raw/master/bin/artifacts/dict-tools.jar -P ~/dict-tools/bin/artifacts/
-make all
